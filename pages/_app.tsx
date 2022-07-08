5d06d6b8f4c7ae4cfd1e1de92556e88cf22ab17f
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-07-08 15:45:00 +0300

    add SessionProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 import Header from '../components/Header'
+import {SessionProvider} from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
