29b904f3cded7ae5ac8d449a02829ced1fb6b377
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-07-08 16:00:34 +0300

    add AuthWrapper

---
+++ components/AuthWrapper.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useSession } from "next-auth/react";
+
+const AuthWrapper = ({ children }: { children: React.ReactNode }) => {
+  const { status } = useSession();
+
+  if (status === "loading") return null;
+
+  return <>{children}</>;
+};
+
+export default AuthWrapper;
